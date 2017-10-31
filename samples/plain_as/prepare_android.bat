6ed4aeb82894ab8ca4f22349c7ef59e92b01574c
Juan Carlos Pazmiño <juankpro@outlook.com>
2017-10-31 07:36:02 -0500

    Add batch files

---
+++ samples/plain_as/prepare_android.bat
@@ -0,0 +1,29 @@
+set PREPARE_SCRIPT_PATH=%~dp0
+
+:: name of APP file
+set APP_NAME=sample
+
+:: Certificate password
+set CERT_FILE=test_android.p12
+set CERT_PASS=1234
+
+set RES_PATH=%EXT_DIR%\..\res
+
+echo ***** COPYING ANE RESOURCES *****
+rmdir /S /Q %RES_PATH%
+xcopy /E /I /Q res %RES_PATH%
+
+call %BUILD_ANE_PATH%
+
+rmdir /S /Q %RES_PATH%
+
+echo ***** ANDROID EMULATOR APP ******
+echo ***** compiling SWF file *****
+
+call %AMXML% -swf-version %AIR_VERSION% ^
+  -define+=CONFIG::device,true ^
+  -define+=CONFIG::iphone,false ^
+  -define+=CONFIG::android,true ^
+  -external-library-path+=%EXT_DIR%\LocalNotificationLib.ane ^
+  -output .\%APP_NAME%.swf ^
+  -- .\Sample.as
